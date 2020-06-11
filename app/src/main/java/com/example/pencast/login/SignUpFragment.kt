d6598341322d30936021c9419c4d65a65a44aeee
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-12 01:42:10 +0530

    Profile Image Upload

--- app/src/main/java/com/example/pencast/login/SignUpFragment.kt
+++ app/src/main/java/com/example/pencast/login/SignUpFragment.kt
@@ -1,6 +1,13 @@
 package com.example.pencast.login
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,9 +20,17 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.pencast.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import java.util.*
 
 class SignUpFragment : Fragment() {
 
+    private val IMAGE_PICKER_REQUEST_CODE = 1
+
+    var selectedPhotoUri: Uri? = null
+
+    lateinit var signUpProfile: Button
     lateinit var userNameEditText: EditText
     lateinit var emailEditText: EditText
     lateinit var passwordEditText: EditText
@@ -26,6 +41,7 @@ class SignUpFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
         val signInAction: TextView = view.findViewById(R.id.action_sign_in)
+        signUpProfile = view.findViewById(R.id.sign_up_profile)
         userNameEditText = view.findViewById(R.id.sign_up_username)
         emailEditText = view.findViewById(R.id.sign_up_email)
         passwordEditText = view.findViewById(R.id.sign_up_password)
@@ -36,11 +52,45 @@ class SignUpFragment : Fragment() {
         signInAction.setOnClickListener {
             findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
         }
+        signUpProfile.setOnClickListener {
+            val imagePickerIntent = Intent(Intent.ACTION_PICK)
+            imagePickerIntent.type = "image/*"
+            startActivityForResult(imagePickerIntent, IMAGE_PICKER_REQUEST_CODE)
+        }
         // Inflate the layout for this fragment
         return view
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            selectedPhotoUri = data.data
+            try {
+                selectedPhotoUri?.let {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        val bitmap = MediaStore.Images.Media.getBitmap(
+                            activity?.contentResolver,
+                            selectedPhotoUri
+                        )
+                        signUpProfile.background = BitmapDrawable(context?.resources, bitmap)
+                    } else {
+                        val source = ImageDecoder.createSource(
+                            requireActivity().contentResolver,
+                            selectedPhotoUri!!
+                        )
+                        val bitmap = ImageDecoder.decodeBitmap(source)
+                        signUpProfile.background = BitmapDrawable(context?.resources, bitmap)
+                    }
+                    signUpProfile.text=""
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     private fun signUpUser() {
+        uploadProfileImage()
         val userName = userNameEditText.text.toString()
         val email = emailEditText.text.toString()
         val password = passwordEditText.text.toString()
@@ -64,6 +114,19 @@ class SignUpFragment : Fragment() {
                     "Failed to create user: ${it.message}",
                     Toast.LENGTH_SHORT
                 ).show()
+            }
+    }
+
+    private fun uploadProfileImage() {
+        if (selectedPhotoUri == null)
+            return
+        val filename = UUID.randomUUID().toString()
+        val storage = FirebaseStorage.getInstance().getReference("/profileImages/$filename")
+        storage.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                storage.downloadUrl.addOnSuccessListener {
+                    Log.d("SignUpFragment", "Location : $it")
+                }
             }
     }
 }
