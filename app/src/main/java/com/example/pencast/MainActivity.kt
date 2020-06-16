5d40acc74487aaf6be21bbb7dc3d7f11e79f26e0
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-16 12:27:10 +0530

    Sign out

--- app/src/main/java/com/example/pencast/MainActivity.kt
+++ app/src/main/java/com/example/pencast/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.pencast
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -16,22 +18,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        verifyUserIsLoggedIn()
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        verifyUserIsLoggedIn()
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_conversations))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_conversations
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-    private fun verifyUserIsLoggedIn(){
-        if (FirebaseAuth.getInstance().uid==null){
-            val intent= Intent(this,LoginActivity::class.java)
-            intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+
+    private fun verifyUserIsLoggedIn() {
+        if (FirebaseAuth.getInstance().uid == null) {
+            moveToLogin()
         }
+    }
+
+    private fun moveToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                moveToLogin()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
