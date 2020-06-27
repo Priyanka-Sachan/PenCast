60624cde42f0b6c3bf6da94c33a96845c89c9e67
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-27 12:38:46 +0530

    Update status.

--- app/src/main/java/com/example/pencast/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/pencast/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.pencast.ui.profile
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
@@ -23,6 +27,8 @@ class ProfileFragment : Fragment() {
     lateinit var authProfileImage: ImageView
     lateinit var authUsername: TextView
     lateinit var authStatus: TextView
+    lateinit var authSaveStatus: ImageButton
+    lateinit var authEditStatus: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,15 +41,51 @@ class ProfileFragment : Fragment() {
 
         authProfileImage = view.findViewById(R.id.auth_profile_image)
         authUsername = view.findViewById(R.id.auth_username)
+
+        authEditStatus = view.findViewById(R.id.auth_edit_status)
+        authEditStatus.setOnClickListener {
+            authStatus.isEnabled = true
+            authEditStatus.visibility = ImageButton.INVISIBLE
+            authSaveStatus.visibility = ImageButton.VISIBLE
+        }
+
+        authSaveStatus = view.findViewById(R.id.auth_save_status)
         authStatus = view.findViewById(R.id.auth_status)
+        authStatus.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                if (charSequence.toString() != user.status)
+                    authSaveStatus.isEnabled = true
+                if (authSaveStatus.isEnabled)
+                    authSaveStatus.setBackgroundResource(R.drawable.ic_circle_enabled)
+                else
+                    authSaveStatus.setBackgroundResource(R.drawable.ic_circle_not_enabled)
+            }
+
+            override fun afterTextChanged(editable: Editable) {}
+        })
+
+        authSaveStatus.setOnClickListener {
+            if (authStatus.text.toString().trim().isNotEmpty())
+                updateStatus(authStatus.text.toString())
+        }
 
         return view
+    }
+
+    private fun updateStatus(status: String) {
+        database.child("status").setValue(status)
+        authEditStatus.visibility = ImageButton.VISIBLE
+        authStatus.isEnabled=false
+        authSaveStatus.setBackgroundResource(R.drawable.ic_circle_not_enabled)
+        authSaveStatus.visibility = ImageButton.INVISIBLE
     }
 
     private fun attachDatabaseListener() {
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                user= dataSnapshot.getValue(User::class.java)!!
+                user = dataSnapshot.getValue(User::class.java)!!
                 Glide.with(authProfileImage.context)
                     .load(user.profileImage)
                     .into(authProfileImage)
